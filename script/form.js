d49d1dced1c19142d53dcbe351fc97829d7f045d
NZeroName <AlexanderSidorov96@yandex.ru>
2024-06-13 22:37:28 +0300

    Сделал валидацию, но надо переработать
    Добавил валидацию для каждого поля. Надо переделать валидацию формы входа ( логин и пароль ), сделать что бы валидация была по нажатию кнопки, а не через фокус

--- script/form.js
+++ script/form.js
@@ -1,4 +1,5 @@
 let formContainer = document.querySelector(".entryForm"); // Контейнер формы
+
 formContainer.addEventListener("click", (e) => {
   e.preventDefault;
   // Форма регистрации
@@ -22,10 +23,11 @@ function SignupTemplate() {
   return `<div class="entryForm__wrapper">
             <p>Sign up</p>
             <form class="entryForm__form" action="#">
-              <div class='name'><input name=" Name" type="name" placeholder="Name" /></div>
-              <div class='email'><input name="E-mail" type="email" placeholder="Email" /></div>
-              <div class='password'><input name="Password" type="password" placeholder="Password" /></div>
+              <div class='name'><input class="entryForm__input" name="Name" type="name" placeholder="Name" /></div>
+              <div class='email'><input class="entryForm__input" name="E-mail" type="email" placeholder="Email" /></div>
+              <div class='password'><input class="entryForm__input" name="Password" type="password" id='password' placeholder="Password" /></div>
               <div class='repeatPassword'><input
+              class="entryForm__input"
                 type="password"
                 placeholder="Repeat password"
                 name="Repeat password"
@@ -51,6 +53,7 @@ function loginTemplate() {
             <form class="entryForm__form" action="#">
               <div class="email">
                 <input
+                class="entryForm__input"
                   type="email"
                   name="E-mail"
                   id="email"
@@ -59,6 +62,7 @@ function loginTemplate() {
               </div>
               <div class="password">
                 <input
+                class="entryForm__input"
                   type="password"
                   placeholder="Password"
                   id="password"
@@ -81,19 +85,63 @@ function inputHint() {
     .querySelector(".entryForm__form")
     .getElementsByTagName("input");
   for (let i = 0; i < inputs.length; i++) {
-    if (!inputs[i].classList.contains("check__input")) {
-      inputs[i].addEventListener("focus", (e) => {
-        return inputs[i].insertAdjacentHTML(
-          "beforebegin",
-          `<div class='input__hint'>${inputs[i].name}</div>`
-        );
-      });
-    }
-    inputs[i].addEventListener("blur", () => {
-      if (inputs[i].value === "") {
-        return document.querySelector(".input__hint").remove();
+    inputs[i].addEventListener("focus", (e) => {
+      e.target.classList.remove("isValid");
+      e.target.classList.remove("notValid");
+      if (
+        !e.target.parentNode.classList.contains("showHint") &&
+        !e.target.classList.contains("check__input")
+      ) {
+        e.target.parentNode.classList.add("showHint");
+        createHint(inputs[i]);
       }
+    });
+
+    inputs[i].addEventListener("blur", (e) => {
+      // Проверка на то, что
+      if (inputs[i].value === "" && inputs[i].classList.contains("notValid")) {
+        return false;
+      } else if (inputs[i].value === "") {
+        e.target.parentNode.classList.remove("showHint");
+        e.target.parentNode.querySelector(".input__hint").remove();
+      } else validaton(inputs[i]);
     });
   }
 }
 inputHint();
+
+function createHint(elem) {
+  return elem.insertAdjacentHTML(
+    "beforebegin",
+    `<div class='input__hint'>${elem.name}</div>`
+  );
+}
+
+// Валидация
+const emailRegex = /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z]{2,}$/;
+const nameRegex = /^[а-яёa-z'-]{2,50}$/i;
+const pasRegex = /^(?=.*[a-z])(?=.*\d)[A-Za-z\d]{8,}$/;
+
+function validaton(input) {
+  switch (input.name) {
+    case "E-mail":
+      emailRegex.test(input.value)
+        ? input.classList.add("isValid")
+        : input.classList.add("notValid");
+      break;
+    case "Password":
+      pasRegex.test(input.value)
+        ? input.classList.add("isValid")
+        : input.classList.add("notValid");
+      break;
+    case "Name":
+      nameRegex.test(input.value)
+        ? input.classList.add("isValid")
+        : input.classList.add("notValid");
+      break;
+    case "Repeat password":
+      input.value === document.getElementById("password").value
+        ? input.classList.add("isValid")
+        : input.classList.add("notValid");
+  }
+}
---
+++ script/validation.js
@@ -0,0 +1,3 @@
+const emailRegex = /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z]{2,}$/;
+const nameRegex = /^[а-яёa-z'-]{2,50}$/i;
+const pasRegex = /^(?=.*[a-z])(?=.*\d)[A-Za-z\d]{8,}$/;
