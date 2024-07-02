b421f42e5464a73caaa71ee5075fd0e717be562e
NZeroName <AlexanderSidorov96@yandex.ru>
2024-07-02 10:45:00 +0300

    Доделал форму входа и регистрации
    Переписал часть кода, сделал его более структурированным. Переделал валидацию, что бы она зависела от названия формы (Логин или Регистрация). Удалил все console.log, сделал редиректы после успешной заполнении формы. Дальше надо придумать где хранить данные регистрации и начинать делать основной функционал

--- script/form.js
+++ script/form.js
@@ -1,128 +1,193 @@
-let formContainer = document.querySelector(".entryForm"); // Контейнер формы
+document.addEventListener("DOMContentLoaded", function () {
+  updateForm(loginTemplate());
+});
+
+const formContainer = document.querySelector(".entryForm");
 
 formContainer.addEventListener("click", (e) => {
-  e.preventDefault;
-  // Форма регистрации
   if (e.target.classList.contains("registration")) {
-    // Удаление первичного шаблона формы
-    formContainer.querySelector(".entryForm__wrapper").remove();
-
-    // Отрисовка формы регистрации и подсказки для полей
-    formContainer.insertAdjacentHTML("beforeend", SignupTemplate());
-    inputHint();
-    // Форма входа
+    updateForm(SignupTemplate());
   } else if (e.target.classList.contains("login")) {
-    // Удаление формы регистрации
-    formContainer.querySelector(".entryForm__wrapper").remove();
-    // Отрисовка формы входа
-    formContainer.insertAdjacentHTML("beforeend", loginTemplate());
-    inputHint();
+    updateForm(loginTemplate());
   }
 });
-function SignupTemplate() {
-  return `<div class="entryForm__wrapper">
-            <p>Sign up</p>
-            <form class="entryForm__form" action="#">
-              <div class='name'><input class="entryForm__input" name="Name" type="name" placeholder="Name" /></div>
-              <div class='email'><input class="entryForm__input" name="E-mail" type="email" placeholder="Email" /></div>
-              <div class='password'><input class="entryForm__input" name="Password" type="password" id='password' placeholder="Password" /></div>
-              <div class='repeatPassword'><input
-              class="entryForm__input"
-                type="password"
-                placeholder="Repeat password"
-                name="Repeat password"
-                id="confirm_password"
-              /></div>
-              <button class="entryForm__btn" type="submit">SignUp</button>
-                <label class='check'>
-                  <input type="checkbox" name="policyPrivacy" id="policy" class='check__input'/>I agree with the&nbsp
-                  <span class='check__box'></span>
-                  <a href='#'>Policy Privacy</a>
-                  </label>
-            </form>
-            <div class="entryForm__signIn">
-              <p>
-                Do you have an account? <button class="login toggleForm">Log in</button>
-              </p>
-            </div>
-          </div>`;
+
+function updateForm(template) {
+  formContainer.innerHTML = template;
+  whatIsForm();
 }
-function loginTemplate() {
-  return `<div class="entryForm__wrapper">
-            <p>Log in</p>
-            <form class="entryForm__form" action="#">
-              <div class="email">
-                <input
-                class="entryForm__input"
-                  type="email"
-                  name="E-mail"
-                  id="email"
-                  placeholder="E-mail"
-                />
-              </div>
-              <div class="password">
-                <input
-                class="entryForm__input"
-                  type="password"
-                  placeholder="Password"
-                  id="password"
-                  name="Password"
-                />
-              </div>
-              <button class="entryForm__btn" type="submit">Log in</button>
-            </form>
-            <div class="entryForm__signIn">
-              <p>
-                Do you have not an account?
-                <button class="registration toggleForm">Sign in</button>
-              </p>
-            </div>
-          </div>`;
+
+function whatIsForm() {
+  const submitBtn = document.querySelector(".entryForm__btn");
+  const formName = document
+    .querySelector(".entryForm__wrapper")
+    .querySelector(".formName").textContent;
+  formValidation(formName);
+
+  submitBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    if (isFormValid()) {
+      redirectToPage();
+    }
+  });
 }
-// отрисовка сносок в инпуте
-function inputHint() {
-  let inputs = document
+
+function isFormValid() {
+  const inputs = document
     .querySelector(".entryForm__form")
     .getElementsByTagName("input");
-  for (let i = 0; i < inputs.length; i++) {
-    inputs[i].addEventListener("focus", (e) => {
-      e.target.classList.remove("isValid");
-      e.target.classList.remove("notValid");
-      if (
-        !e.target.parentNode.classList.contains("showHint") &&
-        !e.target.classList.contains("check__input")
-      ) {
-        e.target.parentNode.classList.add("showHint");
-        createHint(inputs[i]);
-      }
-    });
+  let valid = true;
 
-    inputs[i].addEventListener("blur", (e) => {
-      // Проверка на то, что
-      if (inputs[i].value === "" && inputs[i].classList.contains("notValid")) {
-        return false;
-      } else if (inputs[i].value === "") {
-        e.target.parentNode.classList.remove("showHint");
-        e.target.parentNode.querySelector(".input__hint").remove();
-      } else validaton(inputs[i]);
-    });
+  for (let input of inputs) {
+    if (!input.classList.contains("isValid")) {
+      valid = false;
+      formError(input);
+    }
+  }
+  return valid;
+}
+
+function redirectToPage() {
+  const submitBtn = document.querySelector(".entryForm__btn");
+  if (submitBtn.textContent === "Log in") {
+    window.location.href = "test.html";
+  } else if (submitBtn.textContent === "SignUp") {
+    window.location.href = "index.html";
   }
 }
-inputHint();
+
+function SignupTemplate() {
+  return `
+    <div class="entryForm__wrapper">
+      <p class='formName'>Sign up</p>
+      <form class="entryForm__form" action="#">
+        <div class='name'><input class="entryForm__input" name="Name" type="text" placeholder="Name" /></div>
+        <div class='email'><input class="entryForm__input" name="E-mail" type="email" placeholder="Email" /></div>
+        <div class='password'>
+          <input class="entryForm__input" name="Password" type="password" id='password' placeholder="Password" />
+          <img class="eye hidePas" src="img/eye.svg" alt="Toggle Password Visibility" />
+        </div>
+        <div class='repeatPassword'>
+          <input class="entryForm__input" type="password" placeholder="Repeat password" name="Repeat password" id="confirm_password" />
+        </div>
+        <button class="entryForm__btn btnDisable" type="submit">SignUp</button>
+        <label class='check'>
+          <input type="checkbox" name="policyPrivacy" id="policy" class='check__input'/> I agree with the
+          <span class='check__box'></span>
+          <a href='#'>Policy Privacy</a>
+        </label>
+      </form>
+      <div class="entryForm__signIn">
+        <p>Do you have an account? <button class="login toggleForm">Log in</button></p>
+      </div>
+    </div>`;
+}
+
+function loginTemplate() {
+  return `
+    <div class="entryForm__wrapper">
+      <p class='formName'>Log in</p>
+      <form class="entryForm__form" action="#">
+        <div class="email">
+          <input class="entryForm__input" type="email" name="E-mail" id="email" placeholder="E-mail" />
+        </div>
+        <div class="password">
+          <input class="entryForm__input" type="password" placeholder="Password" id="password" name="Password" />
+          <img class="eye hidePas" src="img/eye.svg" alt="Toggle Password Visibility" />
+        </div>
+        <button class="entryForm__btn" type="submit">Log in</button>
+      </form>
+      <div class="entryForm__signIn">
+        <p>Do you have not an account? <button class="registration toggleForm">Sign in</button></p>
+      </div>
+    </div>`;
+}
+
+function formValidation(formName) {
+  const inputs = document
+    .querySelector(".entryForm__form")
+    .getElementsByTagName("input");
+  if (formName === "Log in") {
+    for (let input of inputs) {
+      hint(input);
+      hideHint(input);
+      formError(input);
+      document
+        .querySelector(".entryForm__btn")
+        .addEventListener("click", () => {
+          validaton(input);
+          formError(input);
+        });
+    }
+  } else if (formName === "Sign up") {
+    for (let input of inputs) {
+      hint(input);
+      hideHint(input);
+      formError(input);
+      input.addEventListener("blur", () => {
+        validaton(input);
+        formError(input);
+        toggleSubmitButtonState();
+      });
+    }
+  }
+  const passwordImg = document.querySelector(".password img");
+  passwordImg.addEventListener("click", () =>
+    togglePasswordVisibility(passwordImg)
+  );
+}
+
+function hint(elem) {
+  elem.addEventListener("focus", (e) => {
+    if (
+      !e.target.parentNode.classList.contains("showHint") &&
+      !e.target.classList.contains("check__input")
+    ) {
+      e.target.parentNode.classList.add("showHint");
+      createHint(elem);
+    }
+  });
+}
+
+function hideHint(elem) {
+  elem.addEventListener("blur", (e) => {
+    if (elem.value === "") {
+      e.target.parentNode.classList.remove("showHint");
+      const hintElem = e.target.parentNode.querySelector(".input__hint");
+      if (hintElem) hintElem.remove();
+    }
+  });
+}
 
 function createHint(elem) {
-  return elem.insertAdjacentHTML(
+  elem.insertAdjacentHTML(
     "beforebegin",
     `<div class='input__hint'>${elem.name}</div>`
   );
 }
 
-// Валидация
-const emailRegex = /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z]{2,}$/;
-const nameRegex = /^[а-яёa-z'-]{2,50}$/i;
-const pasRegex = /^(?=.*[a-z])(?=.*\d)[A-Za-z\d]{8,}$/;
+function formError(elem) {
+  const inputName = elem.name.toLowerCase();
+  const errorElement = elem.parentNode.querySelector(".entryForm__error");
+
+  if (elem.classList.contains("notValid") && !errorElement) {
+    elem.parentNode.insertAdjacentHTML(
+      "beforeend",
+      `<span class='entryForm__error'>Wrong ${inputName}</span>`
+    );
+  } else if (elem.classList.contains("isValid") && errorElement) {
+    errorElement.remove();
+  }
+}
 
 function validaton(input) {
+  const emailRegex = /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z]{2,}$/;
+  const nameRegex = /^[а-яёa-z'-]{2,50}$/i;
+  const pasRegex = /^(?=.*[a-z])(?=.*\d)[A-Za-z\d]{8,}$/;
+
+  input.classList.remove("isValid");
+  input.classList.remove("notValid");
+
   switch (input.name) {
     case "E-mail":
       emailRegex.test(input.value)
@@ -140,8 +205,35 @@ function validaton(input) {
         : input.classList.add("notValid");
       break;
     case "Repeat password":
+      input.value !== "" &&
       input.value === document.getElementById("password").value
         ? input.classList.add("isValid")
         : input.classList.add("notValid");
+      break;
+    case "policyPrivacy":
+      input.checked
+        ? input.classList.add("isValid")
+        : input.classList.add("notValid");
+      break;
+  }
+}
+
+function togglePasswordVisibility(img) {
+  const input = img.previousElementSibling;
+  const isPassword = input.type === "password";
+  input.type = isPassword ? "text" : "password";
+  img.classList.toggle("hidePas", !isPassword);
+  img.classList.toggle("showPas", isPassword);
+}
+
+function toggleSubmitButtonState() {
+  const submitBtn = document.querySelector(".entryForm__btn");
+  const isValidForm = isFormValid();
+  if (isValidForm) {
+    submitBtn.classList.remove("btnDisable");
+    submitBtn.classList.add("btnEnable");
+  } else {
+    submitBtn.classList.remove("btnEnable");
+    submitBtn.classList.add("btnDisable");
   }
 }
--- script/validation.js
+++ script/validation.js
@@ -1,3 +1,35 @@
 const emailRegex = /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z]{2,}$/;
 const nameRegex = /^[а-яёa-z'-]{2,50}$/i;
 const pasRegex = /^(?=.*[a-z])(?=.*\d)[A-Za-z\d]{8,}$/;
+
+function inputHint() {
+  let inputs = document
+    .querySelector(".entryForm__form")
+    .getElementsByTagName("input");
+  for (let i = 0; i < inputs.length; i++) {
+    whatIsForm(inputs[i]);
+    inputs[i].addEventListener("focus", (e) => {
+      if (
+        !e.target.parentNode.classList.contains("showHint") &&
+        !e.target.classList.contains("check__input")
+      ) {
+        e.target.parentNode.classList.add("showHint");
+        createHint(inputs[i]);
+      }
+    });
+
+    inputs[i].addEventListener("blur", (e) => {
+      if (document.querySelector(".entryForm__btn").textContent === "Log in") {
+        whatIsForm(input[i]);
+      } else if (
+        inputs[i].value === "" &&
+        inputs[i].classList.contains("notValid")
+      ) {
+        return false;
+      } else if (inputs[i].value === "") {
+        e.target.parentNode.classList.remove("showHint");
+        e.target.parentNode.querySelector(".input__hint").remove();
+      } else validaton(inputs[i]);
+    });
+  }
+}
